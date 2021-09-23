c1337af66d9a210b2fc80329b090e23ea593040f
Markus Bader <markus.bader@tuwien.ac.at>
2021-09-23 08:32:22 +0200

    MC

---
+++ monte-carlo/Monte_Carlo.py
@@ -0,0 +1,90 @@
+import sys
+import gym
+import numpy as np
+from collections import defaultdict
+
+from plot_utils import plot_blackjack_values, plot_policy
+
+env = gym.make('Blackjack-v1')
+
+
+print(env.observation_space)
+print(env.action_space)
+
+
+for i_episode in range(3):
+    state = env.reset()
+    while True:
+        print(state)
+        action = env.action_space.sample()
+        state, reward, done, info = env.step(action)
+        if done:
+            print('End game! Reward: ', reward)
+            print('You won :)\n') if reward > 0 else print('You lost :(\n')
+            break
+
+
+def generate_episode_from_limit_stochastic(bj_env):
+    episode = []
+    state = bj_env.reset()
+    while True:
+        probs = [0.8, 0.2] if state[0] > 18 else [0.2, 0.8]
+        action = np.random.choice(np.arange(2), p=probs)
+        next_state, reward, done, info = bj_env.step(action)
+        episode.append((state, action, reward))
+        state = next_state
+        if done:
+            break
+    return episode
+
+for i in range(3):
+    print(generate_episode_from_limit_stochastic(env))
+
+
+def mc_prediction_q(env, num_episodes, generate_episode, gamma=1.0):
+    # initialize empty dictionaries of arrays
+    returns_sum = defaultdict(lambda: np.zeros(env.action_space.n))
+    N = defaultdict(lambda: np.zeros(env.action_space.n))
+    Q = defaultdict(lambda: np.zeros(env.action_space.n))
+    # loop over episodes
+    for i_episode in range(1, num_episodes+1):
+        # monitor progress
+        if i_episode % 1000 == 0:
+            print("\rEpisode {}/{}.".format(i_episode, num_episodes), end="")
+            sys.stdout.flush()
+        ## TODO: complete the function
+        episode = generate_episode_from_limit_stochastic(env)
+        for [state, action, reward] in episode:
+            returns_sum[state][action] =  Q[state][action] + reward
+            N[state][action] =  N[state][action] + 1
+            Q[state][action] =  returns_sum[state][action] / N[state][action]
+    return Q
+Q = mc_prediction_q(env, 10, generate_episode_from_limit_stochastic)
+print(Q)
+
+def mc_prediction_q_solution(env, num_episodes, generate_episode, gamma=1.0):
+    # initialize empty dictionaries of arrays
+    returns_sum = defaultdict(lambda: np.zeros(env.action_space.n))
+    N = defaultdict(lambda: np.zeros(env.action_space.n))
+    Q = defaultdict(lambda: np.zeros(env.action_space.n))
+    # loop over episodes
+    for i_episode in range(1, num_episodes+1):
+        # monitor progress
+        if i_episode % 1000 == 0:
+            print("\rEpisode {}/{}.".format(i_episode, num_episodes), end="")
+            sys.stdout.flush()
+        # generate an episode
+        episode = generate_episode(env)
+        # obtain the states, actions, and rewards
+        states, actions, rewards = zip(*episode)
+        # prepare for discounting
+        discounts = np.array([gamma**i for i in range(len(rewards)+1)])
+        # update the sum of the returns, number of visits, and action-value 
+        # function estimates for each state-action pair in the episode
+        for i, state in enumerate(states):
+            returns_sum[state][actions[i]] += sum(rewards[i:]*discounts[:-(1+i)])
+            N[state][actions[i]] += 1.0
+            Q[state][actions[i]] = returns_sum[state][actions[i]] / N[state][actions[i]]
+    return Q
+Q = mc_prediction_q_solution(env, 10, generate_episode_from_limit_stochastic)
+print(Q)
